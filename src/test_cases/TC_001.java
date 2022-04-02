ce2df83d174e8ac03cb9da649bb101f5f0540542
abdulqayoomrahimi <rahimiabdulqayoom@gmail.com>
2022-04-02 09:59:41 -0700

    updated by RAHIMI positive testing

--- src/test_cases/TC_001.java
+++ src/test_cases/TC_001.java
@@ -12,6 +12,54 @@ public class TC_001 {
         // positive testing
 
 
+        /*
+        New project --> java project on the left -> next -> next ->
+B26_Git_Branching --> finish
+
+VCS -> Enable Version Control Integration
+
+Create git ignore
+
+    Option 1) right click on project -> new -> file -> .gitignore
+        type the normal files to ignore
+
+    Option 2) you select the file/s you want to add to git ignore -> right click on the files -> git -> add to gitignore
+
+Commit the changes locally
+
+Sharing the project
+
+    Sharing by going to git -> github -> share on github
+
+        - creates a remote repository on github
+        - link our project with the remote repository
+        - push the changes so far
+
+    or manually:
+
+        - went to github.com -> repositories
+
+        - create a new repository
+
+        - took the link from github, then go to intelliJ, go to git -> manage remotes and add click the plus icon. put the url
+
+        - push the first changes
+
+created a new branch
+
+    right click on master branch -> new branch from master
+    we called the branch our names -> checkout to new branch
+
+    made changes on name branch and then commit
+    made more changes on name branch and then commit
+
+    push the local name branch to the origin name branch
+        (the first time we push a new branch, intelliJ offers to create the origin branch for us)
+
+
+         */
+
+
 
 
     }
